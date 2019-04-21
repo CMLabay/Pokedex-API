9caf13f1396600737a34b8a0a52a8d7484561203
Christina Labay <Christina.m.labay@gmail.com>
2019-04-21 17:17:08 -0500

    add functionality to /types and /pokemon

--- server.js
+++ server.js
@@ -1,15 +1,60 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const POKEDEX = require('./pokedex.json')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-    res.send('Hello, butts!')
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychich`, `Rock`, `Steel`, `Water`]
+app.use(function validateBearerToken(req, res, next) {
+    const bearerToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN
+    //move to the next middleware
+    if(bearerToken !== apiToken){
+        return res.status(401).json({ error: 'Unauthorized request' })    
+    }
+    next()
 })
 
-const PORT = 8001
+function handleGetTypes(req, res){
+    res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
+app.get('/pokemon', handleGetPokemon)
+
+function handleGetPokemon(req, res){
+    const { name, type } = req.query;
+    let results = POKEDEX.pokemon;
+
+    if(name){
+        results = results.filter(pokemon =>
+            pokemon.name.toLowerCase().includes(name.toLowerCase())
+        )
+    }
+    if(type){
+        if(![`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychich`, `Rock`, `Steel`, `Water`].includes(type)){
+            return res
+                .status(400)
+                .send('Type must be one of these: Bug, Dark, Dragon, Electric, Fairy, Fighting, Fire, Flying, Ghost, Grass, Ground, Ice, Normal, Poison, Psychich, Rock, Steel, or Water');
+        }
+    }
+
+    //filter the results
+    if(type){
+        results = results.filter(pokemon => 
+                    pokemon.type.includes(type));
+    }
+    res.json(results);
+    
+}
+
+
+const PORT = 8002
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`)
 })
